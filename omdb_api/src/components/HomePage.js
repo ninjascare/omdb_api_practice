a4b7bf52c30ef5be3c898d3f35aa66e10db27ab7
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2019-02-25 14:04:41 -0500

    aded some more things

--- omdb_api/src/components/HomePage.js
+++ omdb_api/src/components/HomePage.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 export default class HomePage extends Component {
   render() {
     return (
-      <div>
+      <div className='App'>
         <h1>Welcome!!</h1>
+        <Link to="/movies">
+          <button>Go Look for your favorite movies</button>
+        </Link>
       </div>
-    )
+    );
   }
 }
