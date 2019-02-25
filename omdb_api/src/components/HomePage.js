3254aa7b983b82724fc05561e3f76b7e6690b31f
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2019-02-25 17:19:55 -0500

    got some data on the page

--- omdb_api/src/components/HomePage.js
+++ omdb_api/src/components/HomePage.js
@@ -4,7 +4,7 @@ import "../App.css";
 export default class HomePage extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <h1>Welcome!!</h1>
         <Link to="/movies">
           <button>Go Look for your favorite movies</button>
--- omdb_api/src/components/MovieList.js
+++ omdb_api/src/components/MovieList.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import "../App.css";
-
+import axios from "axios";
 
 export default class MovieList extends Component {
+  state = {
+    movieList: []
+  };
+
+  componentDidMount() {
+    const key = "805b29cc";
+    axios.get(`http://www.omdbapi.com/?s=Avengers&apikey=${key}`).then(res => {
+      console.log(res.data.Search);
+      this.setState({
+        movieList: res.data.Search
+      });
+    });
+  }
+
   render() {
+    var moviePoster = this.state.movieList.map((movie, i) => (
+      <div key={i}>
+        <img src={movie.Poster} alt="eeads" />
+      </div>
+    ));
+
     return (
       <div className="App">
         <h1>Omdb Searcher</h1>
         <SearchBar />
+        {moviePoster}
       </div>
     );
   }
