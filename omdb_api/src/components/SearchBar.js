e02f6cad741611a305d324891145addf4cd6c8bc
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2019-03-06 13:24:20 -0500

    added some more logic to my code so that i can keep track of all inputs that go into my search bar

--- omdb_api/src/components/SearchBar.js
+++ omdb_api/src/components/SearchBar.js
@@ -1,17 +1,48 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class SearchBar extends Component {
+  state = {
+    movieList: [],
+    searchInput: {
+      title: ""
+    }
+  };
+
+  componentDidMount() {
+    //     const input = handleSubmit();
+    const key = "805b29cc";
+    axios.get(`http://www.omdbapi.com/?s={input}&apikey=${key}`).then(res => {
+      console.log(res.data.Search);
+      this.setState({
+        movieList: res.data.Search
+      });
+    });
+  }
+
+  handleChange = e => {
+    console.log("name", e.target.name);
+    console.log("value", e.target.value);
+    const newSearch = { ...this.state.searchInput };
+    newSearch[e.target.name] = e.target.value;
+    this.setState({
+      searchInput: newSearch
+    });
+  };
+
   render() {
     return (
       <div>
         <form>
           <input
+            onChange={this.handleChange}
             type="text"
-            name="search"
+            name="title"
             id="search"
+            value={this.state.searchInput.title}
             placeholder="Enter title to search"
           />
-          <button>Search</button>
+          <button type="submit">Search</button>
         </form>
       </div>
     );
